61e52c82c395190ff929b2d93f3a470c4577b2cf
灯火酱 <57972428+denghuojiang@users.noreply.github.com>
2020-09-05 16:56:20 +0800

    添加项目文件。

---
+++ LinkStack/LinkStack.cpp
@@ -0,0 +1,66 @@
+﻿
+#include <iostream>
+using namespace std;
+typedef int ElemType;
+struct Node
+{
+    ElemType data;
+    Node* next=NULL;
+};
+class LinkStack
+{
+public:
+	LinkStack();
+	~LinkStack();
+    int push(ElemType* e);
+    ElemType* gettop();
+    ElemType pop();
+private:
+    Node* top=NULL;
+    int size =NULL;
+};
+
+LinkStack::LinkStack()
+{
+    this->top = new Node;
+    this->size = 0;
+}
+
+LinkStack::~LinkStack()
+{
+    delete top;
+}
+int main()
+{
+    std::cout << "Hello World!\n";
+    LinkStack ls;
+    ElemType i = 23;
+    ElemType e = 1;
+    ls.push(&i);
+    ls.push(&e);
+    cout << *ls.gettop()<<endl;
+    ls.pop();
+    cout<<*ls.gettop();
+}
+int LinkStack::push(ElemType* e)
+{
+    Node* pnew = new Node;
+    pnew->data = *e;
+    pnew->next = this->top;
+    this->top = pnew;
+    this->size++;
+    return 0;
+}
+ElemType* LinkStack::gettop()
+{   
+    return &this->top->data;
+}
+ElemType LinkStack::pop()
+{
+    if (this->size == 0)return NULL;
+    Node* pfree = this->top;
+    this->top = this->top->next;
+    free(pfree);
+    this->size--;
+    return this->top->data;
+}
